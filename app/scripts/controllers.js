9f186a104a5567b9b6c6b0a9ae1262278319dc86
Basile Bruneau <basilebruneau@gmail.com>
2015-04-12 19:30:59 +0200

    Add fonts

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -3,5 +3,15 @@ angular.module('jour-nuit-ctrl', [])
 
 .controller('HomeCtrl', function($scope) {
     console.log('home');
+    $scope.fbLogin = function () {
+        if (!window.cordova) {
+            var appId = "468156286670593";
+            facebookConnectPlugin.browserInit(appId);
+        }
+        facebookConnectPlugin.login( ["email"],
+            function (response) { alert(JSON.stringify(response)) },
+            function (response) { alert(JSON.stringify(response)) }
+        );
+    }
 })
 ;
